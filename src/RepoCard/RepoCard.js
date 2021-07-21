de6fefbbee498f85c6b785d7667aeef762f1b3d4
fadyjan <fady.jan95@gmail.com>
2021-07-21 17:33:47 +0200

    Create #1 Project Components initialization

---
+++ src/RepoCard/RepoCard.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./RepoCard.css"
+
+const RepoCard = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default RepoCard
---
+++ src/WrappingRepoCards/WrappingRepoCards.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import "./WrappingRepoCards.css"
+
+
+const WrappingRepoCards = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default WrappingRepoCards
+
+
+    
+
