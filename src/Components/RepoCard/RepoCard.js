b02e81e5945f8ec16d96257112ce57e1b730ece4
fadyjan <fady.jan95@gmail.com>
2021-07-21 19:57:59 +0200

    Create #2 Main architecture Of each Card

--- src/Components/RepoCard/RepoCard.js
+++ src/Components/RepoCard/RepoCard.js
@@ -1,8 +1,21 @@
-import React from 'react'
-import "./RepoCard.css"
+import React from "react";
+import "./RepoCard.css";
 
 const RepoCard = () => {
-<div></div>
-}
+  return (
+    <div className="RepoContainerCard">
+      <div className="ImageContianer"></div>
+      <div className="RepoContent">
+        <div className="RepoName"> Tensorflow</div>
+        <div className="RepoDescription">An Open Source Machine Learning FrameWork for Everyone http://tensorflow.org</div>
+        <div className="RepoInfo">
+            <label className="NbStars">Starts</label>
+            <label className="NbIssues">Issues</label>
+            <label className="TimeInterval">Submitted 30 dats ago by tensorflow</label>
+            </div>
+      </div>
+    </div>
+  );
+};
 
-export default RepoCard
+export default RepoCard;
