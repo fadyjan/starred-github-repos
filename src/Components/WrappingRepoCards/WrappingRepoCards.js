2f78240111f58bef44c872482351cba79b5eeea0
fadyjan <fady.jan95@gmail.com>
2021-07-22 22:55:24 +0200

    Create #3 Import axios library and fetching data

--- src/Components/WrappingRepoCards/WrappingRepoCards.js
+++ src/Components/WrappingRepoCards/WrappingRepoCards.js
@@ -1,17 +1,32 @@
-import React from 'react'
-import "./WrappingRepoCards.css"
-
+import React, { useState, useEffect } from "react";
+import "./WrappingRepoCards.css";
+import RepoCard from "../RepoCard/RepoCard";
+const axios = require("axios").default;
 
 const WrappingRepoCards = () => {
-    return (
-        <div>
-            
-        </div>
-    )
-}
+  const [repos, setRepos] = useState([]);
 
-export default WrappingRepoCards
+  async function FetchApi() {
+    try {
+      const response = await axios.get(
+        "https://api.github.com/search/repositories?q=created:>2017-10-22&sort=stars&order=desc"
+      );
+      console.log(response.data);
+      setRepos(response.data.items);
+    } catch (error) {
+      console.error(error);
+    }
+  }
 
+  useEffect(() => {
+    FetchApi();
+  }, []);
 
-    
+  return (
+    <div id="CardsCotainer">
 
+    </div>
+  );
+};
+
+export default WrappingRepoCards;
