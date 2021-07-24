cbb2b8efdeefe36db4da97b8c6379d8fda4852d4
fadyjan <fady.jan95@gmail.com>
2021-07-24 15:37:57 +0200

    Create #7 Link Pagination with Repo Cards and Fetching repos according to Clicked Page

--- src/Components/WrappingRepoCards/WrappingRepoCards.js
+++ src/Components/WrappingRepoCards/WrappingRepoCards.js
@@ -3,15 +3,15 @@ import "./WrappingRepoCards.css";
 import RepoCard from "../RepoCard/RepoCard";
 const axios = require("axios").default;
 
-const WrappingRepoCards = () => {
+const WrappingRepoCards = (props) => {
   const [repos, setRepos] = useState([]);
-
+  const clickedPage= props.clickedPage 
   async function FetchApi() {
     try {
       const response = await axios.get(
-        "https://api.github.com/search/repositories?q=created:>2017-10-22&sort=stars&order=desc"
+        `https://api.github.com/search/repositories?q=created:>2017-10-22&sort=stars&order=desc&page=${clickedPage}`
       );
-      console.log(response.data);
+      //   console.log(response.data);
       setRepos(response.data.items);
     } catch (error) {
       console.error(error);
@@ -20,11 +20,11 @@ const WrappingRepoCards = () => {
 
   useEffect(() => {
     FetchApi();
-  }, []);
+  }, [clickedPage]);
 
   return (
     <div id="CardsCotainer">
-                  {repos.map((record) => {
+      {repos.map((record) => {
         return <RepoCard record={record}></RepoCard>;
       })}
     </div>
