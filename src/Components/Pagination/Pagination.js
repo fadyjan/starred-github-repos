9ecbbc019955cfe433feff529f5dcd80f0e24b6a
fadyjan <fady.jan95@gmail.com>
2021-07-24 16:51:13 +0200

    Bug #1 Highlight the Selected pagination Page

--- src/Components/Pagination/Pagination.js
+++ src/Components/Pagination/Pagination.js
@@ -1,21 +1,22 @@
-import React from "react";
+import React , {useState} from "react";
 import Pagination from "react-bootstrap/Pagination";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./Pagination.css";
 
 function PaginationComponent(props) {
+  const [activePage, setActivePage] = useState(1)
   const paginationClicked = (event) => {
     var itemClicked = event.target.text;
     itemClicked = Number(itemClicked)
     props.onSaveHandler(itemClicked);
+    setActivePage(itemClicked)
   };
-  let active = 1;
   let items = [];
   for (let number = 1; number <= 5; number++) {
     items.push(
       <Pagination.Item
         key={number}
-        active={number === active}
+        active={number === activePage}
         onClick={paginationClicked}
       >
         {number}
@@ -23,7 +24,7 @@ function PaginationComponent(props) {
     );
   }
   return (
-    <div>
+    <div id="PaginationContainer">
       <Pagination size="lg">{items}</Pagination>
     </div>
   );
