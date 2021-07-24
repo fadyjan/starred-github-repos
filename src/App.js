9e15d6a0bf280c82d42d546e781e1daa655a4c06
fadyjan <fady.jan95@gmail.com>
2021-07-24 16:52:59 +0200

    Enhance #3 Center The Content insde Page Area

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
-import RepoCard from "./Components/RepoCard/RepoCard"
-import WrappingRepoCards from "./Components/WrappingRepoCards/WrappingRepoCards"
+import logo from "./logo.svg";
+import "./App.css";
+import RepoCard from "./Components/RepoCard/RepoCard";
+import WrappingRepoCards from "./Components/WrappingRepoCards/WrappingRepoCards";
+import PaginationComponent from "./Components/Pagination/Pagination";
+import { useState } from "react";
+
 function App() {
+  const [clickedPage, setClickedPage] = useState(1);
+  const HandlerFunction = (ChildToParentConst) => {
+    setClickedPage(ChildToParentConst);
+  };
+
   return (
     <div className="App">
-        <WrappingRepoCards></WrappingRepoCards>
+      <WrappingRepoCards clickedPage={clickedPage}></WrappingRepoCards>
+      <PaginationComponent
+        onSaveHandler={HandlerFunction}
+      ></PaginationComponent>
     </div>
   );
 }
